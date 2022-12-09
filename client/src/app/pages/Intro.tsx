72aaf7ea38325a9bdd6b5500b1ce1a68ab63d9a1
hmingji <hmingji@hotmail.com>
2022-12-09 18:06:47 +0800

    test

--- client/src/app/pages/Intro.tsx
+++ client/src/app/pages/Intro.tsx
@@ -1,52 +1,55 @@
-import { Button, Container, Grid, Typography } from "@mui/material";
-import PublicHeader from "../layout/Header/PublicHeader";
-import { authService } from "../services/AuthService";
-import { introContent } from "./constants/introContent";
+import { Button, Container, Grid, Typography } from '@mui/material';
+import PublicHeader from '../layout/Header/PublicHeader';
+import { authService } from '../services/AuthService';
+import { introContent } from './constants/introContent';
 
 interface Props {
-    darkMode: boolean,
-    handleThemeChange: () => void;
+  darkMode: boolean;
+  handleThemeChange: () => void;
 }
 
 export default function Intro({ ...props }: Props) {
-    return (
-        <>
-            <PublicHeader {...props} />
+  console.log(process.env.REACT_APP_AUTH_URL);
+  return (
+    <>
+      <PublicHeader {...props} />
 
-            <Container sx={{ mt: 10}}>   
-                <Grid container flexDirection='column' sx={{ mt: 25, mb: 8 }}>
-                    <Grid item sx={{mb: 2}}>
-                        <Typography variant='h2' sx={{ mb: 8 }} align='center'>
-                            {introContent.title}
-                        </Typography>
+      <Container sx={{ mt: 10 }}>
+        <Grid container flexDirection="column" sx={{ mt: 25, mb: 8 }}>
+          <Grid item sx={{ mb: 2 }}>
+            <Typography variant="h2" sx={{ mb: 8 }} align="center">
+              {introContent.title}
+            </Typography>
 
-                        <Typography variant='subtitle2' sx={{ mb: 1 }} align='center'>
-                            {introContent.description}
-                        </Typography>
+            <Typography variant="subtitle2" sx={{ mb: 1 }} align="center">
+              {introContent.description}
+            </Typography>
 
-                        <Typography variant='subtitle2' align='center'>
-                            {introContent.remark}
-                        </Typography> 
-                    </Grid>
+            <Typography variant="subtitle2" align="center">
+              {introContent.remark}
+            </Typography>
+          </Grid>
 
-                    <Button 
-                        variant="contained" 
-                        onClick={() => authService.signinRedirect()} 
-                        sx={{ width: '10rem', display: 'flex', margin: '0 auto', mb: 5 }}
-                    >
-                        Start
-                    </Button>
+          <Button
+            variant="contained"
+            onClick={() => authService.signinRedirect()}
+            sx={{ width: '10rem', display: 'flex', margin: '0 auto', mb: 5 }}
+          >
+            Start
+          </Button>
 
-                    <Grid item sx={{ justifyContent: 'center', display: 'flex'}}>
-                        <img 
-                            src="/images/apps-layout.JPG" 
-                            alt="" 
-                            style={{ width: '100%', boxShadow: 'rgb(38, 57, 77) 0px 20px 30px -10px'}} 
-                        />
-                    </Grid>
-                </Grid>
-            </Container> 
-        </>
-    );
-
+          <Grid item sx={{ justifyContent: 'center', display: 'flex' }}>
+            <img
+              src="/images/apps-layout.JPG"
+              alt=""
+              style={{
+                width: '100%',
+                boxShadow: 'rgb(38, 57, 77) 0px 20px 30px -10px',
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 }
