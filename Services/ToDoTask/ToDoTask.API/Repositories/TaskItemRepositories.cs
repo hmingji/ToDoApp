2e6576f2566393d55139347fc3dbca60d02d8d21
hmingji <hmingji@hotmail.com>
2022-12-09 18:58:51 +0800

    update dbconnStr

--- Services/ToDoTask/ToDoTask.API/Repositories/TaskItemRepositories.cs
+++ Services/ToDoTask/ToDoTask.API/Repositories/TaskItemRepositories.cs
@@ -37,16 +37,15 @@ namespace ToDoTask.API.Repositories
                 var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
                 // Parse connection URL to connection string for Npgsql
-                connUrl = connUrl.Replace("postgres://", string.Empty);
+                 connUrl = connUrl.Replace("postgres://", string.Empty);
                 var pgUserPass = connUrl.Split("@")[0];
-                var pgHostPortDb = connUrl.Split("@")[1];
-                var pgHostPort = pgHostPortDb.Split("/")[0];
-                var pgDb = pgHostPortDb.Split("/")[1];
+                var pgHostDb = connUrl.Split("@")[1];
+                var pgHost = pgHostDb.Split("/")[0];
+                var pgDb = pgHostDb.Split("/")[1];
                 var pgUser = pgUserPass.Split(":")[0];
                 var pgPass = pgUserPass.Split(":")[1];
-                var pgHost = pgHostPort.Split(":")[0];
-                var pgPort = pgHostPort.Split(":")[1];
-
+                var pgPort = 5432;
+                
                 return connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;Trust Server Certificate=true";
             }
         }
